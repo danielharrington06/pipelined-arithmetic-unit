33cd395ea46842922744d4bfcd5a6333a19c1e52
Daniel Harrington <danielharrington06@outlook.com>
2026-09-17 17:48:40 +0100

    made a (a+b)*c arithmetic unit and pipelined 3 sets of inputs

--- src/arithmetic_unit.sv
+++ src/arithmetic_unit.sv
@@ -1,12 +1,18 @@
 module arithmetic_unit (
-    input logic clk,
-    input  logic [31:0] a,
-    input  logic [31:0] b,
+    input logic clk, // clock
+    input logic [31:0] a,
+    input logic [31:0] b,
+    input logic [31:0] c,
     output logic [31:0] y
 );
 
-    always_ff @(posedge clk) begin
-        y <= a + b;
+    logic [31:0] sum_reg;
+    logic [31:0] c_reg;
+
+    always_ff @(posedge clk) begin // on every rising clock signal, store a+b in y
+        sum_reg <= a+b;
+        c_reg <= c;
+        y <= sum_reg * c_reg;
     end
 
 endmodule
--- tb/arithmetic_unit_tb.sv
+++ tb/arithmetic_unit_tb.sv
@@ -3,12 +3,14 @@ module arithmetic_unit_tb;
     logic clk;
     logic [31:0] a;
     logic [31:0] b;
+    logic [31:0] c;
     logic [31:0] y;
 
     arithmetic_unit dut (
         .clk(clk),
         .a(a),
         .b(b),
+        .c(c),
         .y(y)
     );
 
@@ -17,22 +19,49 @@ module arithmetic_unit_tb;
     initial begin
         clk = 0;
 
-        a = 10;
-        b = 20;
+        // input set 1
+        a = 2;
+        b = 3;
+        c = 10;
+
 
         @(posedge clk);
         #1;
-        $display("After first rising edge: y = %0d", y);
+        // first cycle should not be ready yet
+        $display("Cycle 1: y = %0d", y);
 
-        a = 5;
-        b = 7;
-
-        #4;
-        $display("Before next rising edge: y = %0d", y);
+        // Input set 2
+        a = 4;
+        b = 1;
+        c = 7;
 
         @(posedge clk);
         #1;
-        $display("After next rising edge: y = %0d", y);
+        // Second cycle: result from input set 1
+        $display("Cycle 2: y = %0d", y);
+        assert(y == 50)
+            else $error("Cycle 2: expected 50, got %0d", y);
+        
+        // Input set 3
+        a = 6;
+        b = 2;
+        c = 4;
+
+        @(posedge clk);
+        #1;
+
+        // Third cycle: result from input set 2
+        $display("Cycle 3: y = %0d", y);
+        assert(y == 35)
+            else $error("Cycle 3: expected 35, got %0d", y);
+        
+        @(posedge clk);
+        #1;
+
+        // Fourth cycle: result from input set 3
+        $display("Cycle 4: y = %0d", y);
+        assert(y == 32)
+            else $error("Cycle 4: expected 32, got %0d", y);
 
         $finish;
     end
